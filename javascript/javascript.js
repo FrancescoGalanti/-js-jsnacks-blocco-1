3694a930f545390943c13573817a4054ba54fc89
FrancescoGalanti <galanti_francesco>
2020-10-16 14:44:53 +0200

    esercizio rpimo fatto

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -3,11 +3,28 @@
 **/
 
 
- // esercizio primo //
+//  // esercizio primo //
+//
+//
+// // prima variabile //
+// var num1 = parseint(prompt("Inserisci il primo numero") );
+//
+//
+//  // seconda variabile //
+// var num2 = parseint(prompt("Inserisci il secondo numero") );
+//
+//
+//  // controllo //
+//  if(num1 > num2) {
+//    console.log("il primo numero" + num1 + "è maggiore");
+//  }
+//   else if(num1 < num2 ) {
+//    console.log("il secondo numero " + num2 + "è maggiore");
+//  } else{
+//    console.log("Numeri uguali")
+//  }
 
-
-// prima variabile //
- var numerogiocatore1 = parseInt(prompt("Scegli un numero"));
+// secondo esercizio //
 
 
 
