aa8240dbb9d871f1d5b4e18413fed9fe1c5731ab
FrancescoGalanti <galanti_francesco>
2020-10-16 14:28:14 +0200

    inizio

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -3,15 +3,18 @@
 **/
 
 
+ // esercizio primo //
 
-// // prima variabile //
- // var numerogiocatore1 = parseInt(prompt("Scegli un numero"));
- //
- // if(isNaN(numerogiocatore1) == true);{
- //   alert("scrivi un numero ")
- // }
+
+// prima variabile //
+ var numerogiocatore1 = parseInt(prompt("Scegli un numero"));
+
+
+
+
+
+
+
+
  //
  // var numerogiocatore2 = parseInt(prompt("scegli un secondo numero") );
-
-
-// secondo esercizio //
